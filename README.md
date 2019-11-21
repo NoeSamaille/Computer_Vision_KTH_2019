19e548c20237da47fc611cac9ca9f9d72f8d700a
NoeSamaille <Noe.Samaille@ibm.com>
2019-11-21 17:29:38 +0100

    lab 2: parts I & II

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 This repository contains my laboration code and reports for KTH course DD2423 Image Analysis and Computer Vision.
 
-+ `lab_1`: Code and report for lab 1
-+ `lab_1/assigment.m`: script answering all the 20 questions of the lab, to run each part uncomment the corresponding code section.
-+ `lab_1/fftwave.m`: implementation of function `fftwave`.
-+ `lab_1/gaussfft.m`: implementation of the discretized gaussian convolution.
-+ `lab_1/lab1_report.pdf`: written answer sheet for lab 1.
++ `lab1`: Code and report for lab 1
+  + `lab1/lab1_script.m`: script answering all the 20 questions of the lab, to run each part uncomment the corresponding code section.
+  + `lab1/fftwave.m`: implementation of function `fftwave`.
+  + `lab1/gaussfft.m`: implementation of the discretized gaussian convolution.
+  + `lab1/lab1_report.pdf`: written answer sheet for lab 1.
---
+++ lab_2/Lv.m
@@ -0,0 +1,34 @@
+function pixels = Lv(inpic, shape, der_op, smooth)
+
+    if strcmp(der_op, 'centered')
+        % Central differences
+        dxmask = [0 0 0; -0.5 0 0.5; 0 0 0];
+        dymask = dxmask';
+    elseif strcmp(der_op, 'roberts')
+        % Robert's operator
+        dxmask = [1 0; 0 -1];
+        dymask = [0 1; -1 0];
+    elseif strcmp(der_op, 'sobel')
+        % Sobel operator
+        dxmask = [-1 0 1; -2 0 2; -1 0 1];
+        dymask = [-1 -2 -1; 0 0 0; 1 2 1];
+    else
+        % Default operator: simple
+        dxmask = [-1 1; 0 0];
+        dymask = dxmask';
+    end
+    
+    if (nargin < 2)
+        shape = 'same';
+    end
+
+    if (smooth == true)
+        % Apply gaussian smoothing
+        scale = 1.0;
+        inpic = gaussfft(inpic, scale);
+    end
+
+    Lx = filter2(dxmask, inpic, shape);
+    Ly = filter2(dymask, inpic, shape);
+    pixels = Lx.^2 + Ly.^2;
+end
