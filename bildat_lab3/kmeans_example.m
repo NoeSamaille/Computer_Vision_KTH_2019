daadbf87dd010f549d6bd2d3648bfdb68577207d
Danny Qiu <danny.qiu@grenoble-inp.org>
2019-12-02 19:13:01 +0100

    lab3 kmeans_segm.m implementation

---
+++ bildat_lab3/kmeans_example.m
@@ -0,0 +1,26 @@
+pkg load image
+pkg load statistics
+addpath Functions
+addpath bildat_lab3
+
+K = 8;               % number of clusters used
+L = 10;              % number of iterations
+seed = 14;           % seed used for random initialization
+scale_factor = 1.0;  % image downscale factor
+image_sigma = 1.0;   % image preblurring scale
+
+I = imread('bildat_lab3/orange.jpg');
+I = imresize(I, scale_factor);
+Iback = I;
+d = 2*ceil(image_sigma*2) + 1;
+h = fspecial('gaussian', [d d], image_sigma);
+I = imfilter(I, h);
+
+tic
+[ segm, centers ] = kmeans_segm(I, K, L, seed);
+toc
+Inew = mean_segments(Iback, segm);
+I = overlay_bounds(Iback, segm);
+imwrite(Inew,'bildat_lab3/result/kmeans1.png')
+imwrite(I,'bildat_lab3/result/kmeans2.png')
+
---
+++ lab_3/kmeans_segm.m
@@ -0,0 +1,34 @@
+function [segmentation, centers] = kmeans_segm(image, K, L, seed)
+  rand("seed", seed);
+  image = double(image);
+  [width, height, ~] = size(image);
+  Ivec = reshape(image, width*height, 3);
+  segmentation = zeros(1, width*height);
+  % Let X be a set of pixels and V be a set of K cluster centers in 3D (R,G,B).
+  % Randomly initialize the K cluster centers
+  centers = randi(255, K, 3);
+  
+  % Compute all distances between pixels and cluster centers
+  d = pdist2(Ivec, centers); # column k = distances between cluster k and all the image pixels
+  
+  % Iterate L times
+  for i=1:L
+    % Assign each pixel to the cluster center for which the distance is minimum
+    [~, k] = max(d, [], 2); # get argmax of each row of d matrix 
+    segmentation = transpose(k);
+    
+    % Recompute each cluster center by taking the mean of all pixels assigned to it
+    for k=1:K
+      cluster = Ivec(segmentation == k, :);
+      if size(cluster, 1) > 0
+        centers(k, :) = mean(cluster);
+      endif
+    endfor
+    
+    % Recompute all distances between pixels and cluster centers
+    d = pdist2(Ivec, centers);
+  endfor
+  
+  # reshape to real form
+  segmentation = reshape(segmentation, width, height);
+end
