c46589221b3197aa8d858b568f43a66e13b37a95
NoeSamaille <Noe.Samaille@ibm.com>
2019-12-13 09:55:21 +0100

    fix comments

--- lab_3/lab3_script.m
+++ lab_3/lab3_script.m
@@ -21,14 +21,14 @@
 
 % Part 4: Normalized cut
 
-##colour_bandwidth = 10.0; % color bandwidth
-##radius = 3;              % maximum neighbourhood distance
-##ncuts_thresh = 2;        % cutting threshold
-##min_area = 300;          % minimum area of segment
-##max_depth = 8;           % maximum splitting depth
-##scale_factor = 0.4;      % image downscale factor
-##image_sigma = 2.0;       % image preblurring scale
-##norm_cut('tiger1', colour_bandwidth, radius, ncuts_thresh, min_area, max_depth, scale_factor, image_sigma)
+% colour_bandwidth = 10.0; % color bandwidth
+% radius = 3;              % maximum neighbourhood distance
+% ncuts_thresh = 2;        % cutting threshold
+% min_area = 300;          % minimum area of segment
+% max_depth = 8;           % maximum splitting depth
+% scale_factor = 0.4;      % image downscale factor
+% image_sigma = 2.0;       % image preblurring scale
+% norm_cut('tiger1', colour_bandwidth, radius, ncuts_thresh, min_area, max_depth, scale_factor, image_sigma)
 
 % Part 5: Graph cut
 
